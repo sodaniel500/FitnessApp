c88f5e7b3bf4f8ec73090bfcb5a90947ddd38a6f
D <sowemimodaniel6@gmail.com>
2022-11-22 13:49:00 +0100

    update on the app

--- src/screen/Plan.js
+++ src/screen/Plan.js
@@ -1,8 +1,27 @@
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
+import { useState, useEffect } from 'react';
 
 const Plan = ({ navigation }) => {
+
+  // const [selectBtn, setSelectBtn] = useState("")
+  const [changeColor, setChangeColor] = useState(false)
+  const [button, setButton] = useState(true)
+  const [tabView, setTabView] = useState("tab1")
+
+  const Change = () => {
+    setChangeColor(!changeColor)
+  }
+
+  const bts = () => {
+    setButton(!button)
+  }
+
+  const changeTab = (value) => {
+    setTabView(value)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.back}>
@@ -19,43 +38,102 @@ const Plan = ({ navigation }) => {
         </View>
       </View>
 
-      <View style={{flexDirection: 'row',  justifyContent: 'space-between'}}>
-        <View style={styles.btn}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TouchableOpacity style={{ ...styles.btn, backgroundColor: changeColor ? '#DDA0DD' : '#FFC0CB' }} onPress={() => { Change() }}>
           <Text style={{ fontSize: 15, fontWeight: 'bold', paddingVertical: 35, }}
           >Weekly</Text>
           <Text style={{ fontWeight: 'bold', fontSize: 25 }}>$840</Text>
           <Text style={{ fontWeight: 'bold', fontSize: 10 }}>Meal per day $120</Text>
-        </View>
+        </TouchableOpacity>
 
-        <View style={{...styles.btn, backgroundColor: '#FFC0CB',}}>
+        <TouchableOpacity style={{ ...styles.btn, backgroundColor: !changeColor ? '#DDA0DD' : '#FFC0CB', }} onPress={() => { Change() }}>
           <Text style={{ fontSize: 15, fontWeight: 'bold', paddingVertical: 35, }}
           >Monthly</Text>
           <Text style={{ fontWeight: 'bold', fontSize: 25 }}>$3360</Text>
           <Text style={{ fontWeight: 'bold', fontSize: 10 }}>Meal per day $524</Text>
-        </View>
-      </View>
-
-      <View style={{marginTop: 30}}>
-        <Text style={{fontSize: 15, fontWeight: 'bold'}}>Start data</Text>
-      </View>
-
-      <View style={styles.bttn}>
-       <Text style={{color: 'white', fontSize: 17, fontWeight: 'bold'}}>Jan 24,2020</Text>
-       <TouchableOpacity>
-       <Text style={{color: 'orange', fontSize: 13, fontWeight: 'bold'}}>Change Data</Text>
-       </TouchableOpacity>
-      </View>
-
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: 20}}>
-        <Text style={{fontSize: 17, fontWeight: 'bold'}}>Select Diet</Text>
-        <TouchableOpacity>
-        <Text style={{color: 'gray',fontWeight: 'bold'}}>See All</Text>
         </TouchableOpacity>
       </View>
 
-      <View>
-        <Text style={styles.img}>Paleo Diet</Text>
+      <View style={{ marginTop: 30 }}>
+        <Text style={{ fontSize: 15, fontWeight: 'bold' }}>Start data</Text>
       </View>
+
+      <View style={styles.bttn}>
+        <Text style={{ color: 'white', fontSize: 17, fontWeight: 'bold' }}>Jan 24,2020</Text>
+        <TouchableOpacity>
+          <Text style={{ color: 'orange', fontSize: 13, fontWeight: 'bold' }}>Change Data</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+        <Text style={{ fontSize: 17, fontWeight: 'bold' }}>Select Diet</Text>
+        {/* <TouchableOpacity onPress={() => {changeTab("tab1")}}> */}
+        <Text style={{ color: 'gray', fontWeight: 'bold' }}>See All</Text>
+        {/* </TouchableOpacity>
+        <TouchableOpacity onPress={() => {changeTab("tab2")}}>
+          <Text style={{ color: 'gray', fontWeight: 'bold' }}>See All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {changeTab("tab3")}}>
+          <Text style={{ color: 'gray', fontWeight: 'bold' }}>See All</Text>
+        </TouchableOpacity> */}
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+        <TouchableOpacity style={{ ...styles.img, backgroundColor: tabView === "tab1" ? '#DDA0DD' : 'white',
+        borderWidth: tabView === "tab1"  ? null : 1 }}
+          onPress={() => {
+            changeTab("tab1")
+            bts()
+          }}>
+          <Text style={{ alignSelf: 'center' }}>Paleo Diet</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ ...styles.imgg, backgroundColor: tabView === "tab2"  ? '#DDA0DD' : 'white', 
+        borderWidth: tabView === "tab2"  ? null : 1 }}
+          onPress={() => {
+            changeTab("tab2")
+            bts()
+          }}>
+          <Text style={{ alignSelf: 'center' }}>Vegan Diet</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ ...styles.imggg, backgroundColor: tabView === "tab3"  ? '#DDA0DD' : 'white',
+         borderWidth: tabView === "tab3"  ? null : 1 }}
+          onPress={() => {
+            changeTab("tab3")
+            bts()
+          }}>
+          <Text style={{ alignSelf: 'center' }}>Regular Diet</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      {tabView === "tab1" ? (<>
+        <View>
+          <Image
+            source={require('../../assets/5.jpg')}
+            style={styles.tab}
+          />
+        </View>
+      </>) : tabView === "tab2" ? (
+        <>
+          <View>
+            <Image
+              source={require('../../assets/6.jpg')}
+              style={styles.tab}
+            />
+          </View>
+        </>
+      ) : tabView === "tab3" ? (
+        <>
+          <View>
+            <Image
+              source={require('../../assets/4.jpg')}
+              style={styles.tab} />
+          </View>
+        </>
+      ) : null
+      }
 
     </SafeAreaView>
   )
@@ -88,5 +166,34 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     borderRadius: 15,
     marginTop: 10
+  },
+  img: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 35,
+    width: 75,
+    borderRadius: 10,
+  },
+  imgg: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 35,
+    width: 75,
+    borderRadius: 10,
+  },
+  imggg: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 35,
+    width: 75,
+    borderRadius: 10,
+  },
+  tab: {
+    padding: 10,
+    height: 150,
+    width: 300,
+    borderRadius: 10,
+    marginTop: 25,
+    resizeMode: 'contain',
   }
 })
